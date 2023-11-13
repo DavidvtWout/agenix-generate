36c300f1272026433c71c4c927344ed0e75f1100
david <david@yoctobyte.nl>
2023-11-13 21:58:10 +0100

    update agenix-check to use hashes

--- pkgs/agenix-check/check-meta.nix
+++ pkgs/agenix-check/check-meta.nix
@@ -7,20 +7,36 @@ let
   else
     throw "check-meta: ${metaPath} does not exist!";
 
-  secretWithDefaults = s:
-    let g = s.generator or { };
-    in s // {
-      publicKeys = s.publicKeys or [ ];
-      generator = {
-        args = { };
-        dependencies = [ ];
-        followArgs = false;
-        followDeps = true;
-      } // g;
-    };
+  hash = x: builtins.hashString "sha256" (builtins.toJSON x);
 
-  secret = secretWithDefaults secrets.${secretName};
-  secretMeta = secretWithDefaults (meta.${secretName} or { });
+  secret = let
+    s = secrets.${secretName};
+    g = s.generator or { };
+  in s // {
+    publicKeys = hash (s.publicKeys or [ ]);
+    generator = {
+      name = g.name or null;
+      args = hash (g.args or { });
+      dependencies = hash (g.dependencies or [ ]);
+      followArgs = g.followArgs or false;
+      followDeps = g.followDeps or true;
+    };
+  };
+
+  secretMeta = let
+    s = meta.${secretName} or { };
+    g = s.generator or { };
+  in s // {
+    publicKeys = s.publicKeys or (hash [ ]);
+    generator = {
+      name = g.name or null;
+      args = g.args or (hash { });
+      dependencies = g.dependencies or (hash [ ]);
+      followArgs = g.followArgs or false;
+      followDeps = g.followDeps or true;
+    };
+  };
+
 in rec {
   needRekey = !inMeta || pubkeysChanged;
   needRegen = regenBecauseArgs || regenBecauseDeps;
@@ -29,7 +45,8 @@ in rec {
   inMeta = builtins.hasAttr secretName meta;
   pubkeysChanged = secret.publicKeys != secretMeta.publicKeys;
   regenBecauseArgs = inMeta && hasGenerator && secret.generator.followArgs
-    && (secret.generator.args != secretMeta.generator.args);
+    && (secret.generator.name != secretMeta.generator.name
+      || (secret.generator.args != secretMeta.generator.args));
   regenBecauseDeps = inMeta && hasGenerator && secret.generator.followDeps
     && (secret.generator.dependencies != secretMeta.generator.dependencies);
 }
