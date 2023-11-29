1f6b7eb687fb8594e4ababd4baf7882421c3bd00
david <david@yoctobyte.nl>
2023-11-29 10:49:06 +0100

    rust: create hello world

--- flake.nix
+++ flake.nix
@@ -1,17 +1,49 @@
-{
+rec {
   description = "agenix secret generator extension";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      formatter = nixpkgs.legacyPackages.${system}.nixfmt;
+    let
+      inherit (builtins) substring;
+      inherit (nixpkgs) lib;
 
-      packages.agenix-check =
-        nixpkgs.legacyPackages.${system}.callPackage ./pkgs/agenix-check { };
-      packages.agenix-generate =
-        nixpkgs.legacyPackages.${system}.callPackage ./pkgs/agenix-generate { };
-      packages.default = self.packages.${system}.agenix-generate;
-    });
+      mtime = self.lastModifiedDate;
+      date =
+        "${substring 0 4 mtime}-${substring 4 2 mtime}-${substring 6 2 mtime}";
+      # rev = self.rev or (throw "Git changes are not committed");
+
+      mkAgenixGenerate = { rustPlatform, nixUnstable, ... }:
+        rustPlatform.buildRustPackage {
+          pname = "agenix-generate";
+          version = "unstable-${date}";
+          src = self;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = false;
+          };
+
+          nativeBuildInputs = [ nixUnstable.out ];
+
+          meta = {
+            inherit description;
+            homepage = "https://github.com/DavidvtWout/agenix-generate";
+            license = [ lib.licenses.cc0 ];
+            mainProgram = "agenix-generate";
+          };
+        };
+
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.nixfmt;
+
+        packages = rec {
+          default = agenix-generate;
+          agenix-generate = pkgs.callPackage mkAgenixGenerate { };
+          # agenix-check = pkgs.callPackage ./pkgs/agenix-check { };
+        };
+      });
 }
