128cc4977bdb82facabfd8aa13c4bbac30a89394
david <david@yoctobyte.nl>
2023-11-29 12:21:21 +0100

    rust: use clap for cli argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   #[clap(short, long, value_parser, env = "AGENIX_IDENTITY")]
+   identity: PathBuf,
+
+   #[clap(short, long, value_parser, env = "AGENIX_META")]
+   meta: Option<PathBuf>,
+
+   #[clap(long, value_parser, env = "AGENIX_RULES", default_value = "./secrets.nix")]
+   rules: PathBuf,
+
+   #[clap(short, long, action)]
+   force: bool,
+
+   #[clap(short, long, action)]
+   all: bool,
+
+   #[clap(index = 1)]
+   filename: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = Args::parse();
+
+   println!("{:?}", args);
 }
